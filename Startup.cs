1be76f751103b6b42a1e75fe60d5d1046d9e2808
Rasha Ali <rashaali84@gmail.com>
2022-04-21 11:16:09 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecurityAuthentication.Startup))]
+namespace SecurityAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
